ac6a0242f1e81764b22b6d59c64510b266122adc
Sree_mugesh <50001213@mahindra.com>
2022-06-29 16:39:31 +0530

    change in spring application

--- javatraining/Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM gcr.io/distroless/java:8
-
-WORKDIR /app
-
-COPY target/*.war /app/app.war
-
-ENTRYPOINT ["java", "-jar", "app.war"]
--- javatraining/src/Stackinterface.java
+++
@@ -1,8 +0,0 @@
-import java.util.Stack;
-
-interface Stackinterface {
-    void isemptyStack(Stack stack);
-    void sizeStack(Stack stack);
-    void addStack(Stack stack ,int a);
-    void popStack(Stack stack );
-}
